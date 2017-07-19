f0aa57f170816734d005b6d9f0e20727fa489a3d
pinon <ymc9.knit.p23@gmail.com>
2017-07-19 11:03:56 +0900

    topのCSSちょっといじった:wq

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>らんだむ</title>
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/champ.css">
+    <link rel="stylesheet" href="/css/top.css">
   </head>
   <body>
     <div class="wrapper">
@@ -14,19 +14,23 @@
             <span class="red">trollプレイでreportされても一切責任を負いません<span>
         </p>
         <form action="create_builds.php" method="POST">
-            <div>
-                <select name="menber">
+            <div class="form_parts">
+                <h3><label for="member"> 人数 </label></h3>
+                <select name="menber" id="menber">
                     <?php for($i = 1 ; $i <= 10 ; $i++): ?>
                     <option><?= $i ?></option>
                     <?php endfor; ?>
                 </select>
             </div>
-            <div>
+            <div class="form_parts">
+                <h3>map</h3>
                 <label><input type="radio" name="map" value="sf" checked="checked">summoners lift</label>
                 <label><input type="radio" name="map" value="ha" >howling abyss</label>
 
             </div>
-            <button type ="submit">build!</button>
+            <div class="submit_wrapper">
+                <button type ="submit">build!</button>
+            </div>
         </form>
     </div>
  </body>
