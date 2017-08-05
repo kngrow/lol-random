005ea6996a2d497d1bb742bf9f0d64c69ec613d5
pinon <ymc9.knit.p23@gmail.com>
2017-08-05 19:50:13 +0900

    ハリケーンとかで再抽選時にすでに保持しているとアレだったので、数値で持つようにした / icheckで装飾

--- create_builds.php
+++ create_builds.php
@@ -6,6 +6,7 @@ $rc = new RandomClass();
 if (isset($_GET['hash'])) {
   $random_list = $rc->decode($_GET['hash']);
   $url = $_GET['hash'];
+  $half = ( count($random_list) / 2 ) - 1;
 } else if( ( isset($_POST['champs']) ||  isset( $_POST['menber'] ) ) && isset( $_POST['map'] ) ) {
   $number = isset($_POST['menber']) ? $_POST['menber'] + 0 : null;
   $map = $_POST['map'];
@@ -37,39 +38,38 @@ if (isset($_GET['hash'])) {
   <?php if($random_list): ?>
   <?php foreach ($random_list as $menber => $champ): ?>
   <div class="builds <?= $menber > $half ? 'red_team' : '' ?>">
-      <div class="champ">
-        <img src="http://ddragon.leagueoflegends.com/cdn/<?= $rc->version ?>/img/champion/<?= $champ['champ']['image']['full'] ?>" alt="">
-        <span class="champ-name"><?= $champ['champ']['name'] ?></span>
-      </div>
-      <div class="item-list">
-        <?php foreach ($champ['build'] as $key => $value): ?>
-          <div class="items">
-          <div class="item-img" style="height:<?= $value['image']['h'] ?>px; width:<?= $value['image']['w'] ?>px; background: url('//ddragon.leagueoflegends.com/cdn/<?= $rc->version ?>/img/sprite/<?= $value['image']['sprite'] ?>') -<?= $value['image']['x'] ?>px -<?=   $value['image']['y'] ?>px no-repeat;">
-          </div>
-          <span class="name"><?= $value['name'] ?></span>
-          <!--
-          <p>
-            <?php foreach ($value['from'] as $k => $v): ?>
-              <img src="http://ddragon.leagueoflegends.com/cdn/<?= $rc->version ?>/img/item/<?= $v ?>.png" style="width:40px" >
-
-            <?php endforeach; ?>
-          </p>
-          -->
-        </div>
-        <?php endforeach; ?>
-      </div>
+    <div class="champ">
+      <img src="http://ddragon.leagueoflegends.com/cdn/<?= $rc->version ?>/img/champion/<?= $champ['champ']['image']['full'] ?>" alt="">
+      <span class="champ-name"><?= $champ['champ']['name'] ?></span>
     </div>
-    <?php if($half === $menber): ?>
-    <hr>
-    <?php endif; ?>
-  <?php endforeach; ?>
-  <?php else: ?>
-  <p>存在しないデータっす</p>
-  <?php endif; ?>
-
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js"></script>
-  <script src="./js/script.js"></script>
+    <div class="item-list">
+      <?php foreach ($champ['build'] as $key => $value): ?>
+      <div class="items">
+      <div class="item-img" title="<?= $value['name'] ?>" style="height:<?= $value['image']['h'] ?>px; width:<?= $value['image']['w'] ?>px; background: url('//ddragon.leagueoflegends.com/cdn/<?= $rc->version ?>/img/sprite/<?= $value['image']['sprite'] ?>') -<?= $value['image']['x'] ?>px -<?=   $value['image']['y'] ?>px no-repeat;">
+      </div>
+      <span class="name"><?= $value['name'] ?></span>
+      <!--
+      <p>
+      <?php foreach ($value['from'] as $k => $v): ?>
+      <img src="http://ddragon.leagueoflegends.com/cdn/<?= $rc->version ?>/img/item/<?= $v ?>.png" style="width:40px" >
+      <?php endforeach; ?>
+      </p>
+      -->
+    </div>
+    <?php endforeach; ?>
   </div>
+</div>
+<?php if($half === $menber): ?>
+  <hr>
+<?php endif; ?>
+<?php endforeach; ?>
+<?php else: ?>
+<p>存在しないデータっす</p>
+<?php endif; ?>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js"></script>
+<script src="./js/script.js"></script>
+</div>
 </body>
 </html>
